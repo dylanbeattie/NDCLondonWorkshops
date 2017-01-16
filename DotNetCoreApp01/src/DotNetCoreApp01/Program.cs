5f62a1595b201e8f4e545e6c83311c2aa7f9fb6b
dylanbeattie <dylan@dylanbeattie.net>
2017-01-16 10:58:20 +0000

    Working minimal .NET Core Kestrel web app with static file support.
    - edited project.json to add dependency on Kestrel web server
    - Created Startup.cs class with UseStaticFiles
    - Created wwwroot folder containing index.html file

---
+++ DotNetCoreApp01/src/DotNetCoreApp01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace DotNetCoreApp01 {
+    public class Program {
+        public static void Main(string[] args) {
+            var builder = new WebHostBuilder()
+                .UseUrls("http://localhost:5000")
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>();
+
+            var server = builder.Build();
+            server.Start();
+            Console.WriteLine("Running server...");
+            Console.ReadKey(false);
+        }
+    }
+
+    public class Startup {
+        public void Configure(IApplicationBuilder app) {
+            app.Use(async (ctx, next) => {
+                Console.WriteLine(ctx.Request.Path);
+                await next();
+            });
+            app.UseStaticFiles();
+            app.Run(ctx => ctx.Response.WriteAsync("Hello World!"));
+        }
+    }
+}
