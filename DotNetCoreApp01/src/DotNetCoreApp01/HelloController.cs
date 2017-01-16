c120c69aec138bb89f4c51b788029c9a3140cdd0
dylanbeattie <dylan@dylanbeattie.net>
2017-01-16 12:20:00 +0000

    Added MVC authentication and CookieAuthentication code

--- DotNetCoreApp01/src/DotNetCoreApp01/HelloController.cs
+++ DotNetCoreApp01/src/DotNetCoreApp01/HelloController.cs
@@ -1,7 +1,23 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DotNetCoreApp01 {
-    public class HelloController {
-        public string Index(string name) {
-            return $"hello {name}";
+
+    public class HelloController : Controller {
+        public ActionResult Index(string name) {
+            return Content($"hello {name}");
+        }
+
+        [Authorize]
+        public ActionResult Secure() {
+            return (Content("Welcome to the secure thing!"));
+
+        }
+    }
+
+    public class AccountController : Controller {
+        public ActionResult Login() {
+            return Content("TODO: login page");
         }
     }
 }
--- DotNetCoreApp01/src/DotNetCoreApp01/Startup.cs
+++ DotNetCoreApp01/src/DotNetCoreApp01/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DotNetCoreApp01 {
@@ -12,14 +14,33 @@ namespace DotNetCoreApp01 {
         }
 
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc();
+
+            var defaultPolicy =
+                new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+            services.AddMvc(setup => setup.Filters.Add(new AuthorizeFilter(defaultPolicy)));
         }
         public void Configure(IApplicationBuilder app) {
+
+            app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
             app.UseLogging(new LoggingOptions() { Message = "DylanLog" });
             app.UseStaticFiles();
-            app.UseMvc(routes => {
-                routes.MapRoute("default", "{controller=Home}/{action=Index}/{name?}");
+
+
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationScheme = "Cookies",
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                LoginPath = new PathString("/account/login"),
+                AccessDeniedPath = new PathString("/account/denied")
             });
+
+            app.UseMvcWithDefaultRoute();
+
+
             app.Run(ctx => ctx.Response.WriteAsync("Hello World!"));
         }
     }
